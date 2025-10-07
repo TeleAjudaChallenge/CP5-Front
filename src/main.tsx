0e7c1fcb46e77d6f73def12f4ce6a284eee6b43e
Matheussansao <matheussansao@hotmail.com>
2025-10-07 19:24:28 -0300

    instalado e adicionado o tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import "./globals.css";
-
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import Login from './routes/Login/index.tsx';
 import Cadastro from './routes/Cadastro/index.tsx';
 import Error from './routes/Error/index.tsx';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import "./globals.css";
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/> , children:[
