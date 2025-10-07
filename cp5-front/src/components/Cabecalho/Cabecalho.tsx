a38d12aefef8f3159f1ccaf992816562ab88416e
Julia de Altino <julia.correa.souza@gmail.com>
2025-10-07 11:18:39 -0300

    alterado cabeçalho para receber as informações do usuario para aparecer em todas as paginas

--- cp5-front/src/components/Cabecalho/Cabecalho.tsx
+++ cp5-front/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,28 @@
 import Menu from "../Menu/Menu";
+import { useNavigate } from "react-router-dom";
 
 export default function Cabecalho() {
+  const navigate = useNavigate();
 
+  const user = JSON.parse(sessionStorage.getItem("auth:user") || "null");
+
+  function handleLogout() {
+    sessionStorage.removeItem("auth:user");
+    navigate("/");
+  }
 
   return (
     <header>
       <h1>Meu App</h1>
-      <Menu/>
+      <Menu />
+      {user ? (
+        <div>
+          <span>{user.nomeUsuario}</span> • <span>{user.email}</span>{" "}
+          <button onClick={handleLogout}>Sair</button>
+        </div>
+      ) : (
+        <div>Faça login</div>
+      )}
     </header>
   );
 }
