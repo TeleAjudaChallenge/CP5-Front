e5d512a28dab265de31d77e3ae7c386f08747000
Matheussansao <matheussansao@hotmail.com>
2025-10-07 19:42:27 -0300

    Foi ocultada o cabeçalho e rodapé nas paginas de autentificação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
 
-export default function App(){
+export default function App() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/" || location.pathname === "/cadastro";
+  
+  if (isAuthPage) {
+    return <Outlet />;
+  }
 
-  return(
+  return (
     <div className="container">
-        <Cabecalho/>
-        <Outlet/>
-        <Rodape/>
+      <Cabecalho />
+      <main className="main">
+        <Outlet />
+      </main>
+      <Rodape />
     </div>
   );
 }
